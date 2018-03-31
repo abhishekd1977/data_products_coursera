a8dbc065e6800657427ffd61cff294afcd28bffc
Abhishek Dubey <abhishekdubey@Abhisheks-MacBook-Pro.local>
2018-03-31 07:44:32 +0530

    week 4 assignment

---
+++ server.R
@@ -0,0 +1,32 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2) # visualization
+library(ggthemes) # visualization
+#setwd("/Users/abhishekdubey/Desktop/Learning/Coursera/DevelopingDataProducts/week_4_assignment")
+setwd("/srv/connect/apps/data_products_coursera")
+getwd()
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
